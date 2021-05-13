a79bf0ac2aac6bf905ecc2743443ee584efd5d9b
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-05-13 18:43:40 +0200

    initial version of application pushed

---
+++ app/controllers/texts_controller.rb
@@ -0,0 +1,53 @@
+class TextsController < ApplicationController
+  require 'uri'
+require 'net/http'
+require 'openssl'
+require'json'
+
+class TextsController < ApplicationController
+  @@parsed_texts = []
+
+  def index
+  end
+
+  def rewrite_text
+    paraphrased_text = api_call(params[:paraphrase][:text])
+    @@parsed_texts << paraphrased_text
+    redirect_to texts_path
+  end
+
+
+  def show_texts
+    @texts = @@parsed_texts
+  end
+
+  def delete_texts
+    @@parsed_texts.clear
+    redirect_to texts_path
+  end
+
+  private
+
+  def api_call(text)
+    url = URI("https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(url)
+    request["content-type"] = 'application/json'
+    request["x-rapidapi-key"] = '0ff14fe9e7msh77b3e0c40e4e814p1d4094jsn1d9333d48168'
+    request["x-rapidapi-host"] = 'rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com'
+    request.body = "{
+        \"language\": \"en\",
+        \"strength\": 3,
+        \"text\": \"#{text}\"
+    }"
+    response = http.request(request)
+    parsed = JSON.parse(response.body)
+  return parsed["rewrite"]
+  end
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'texts#index'
+  resources :texts, only: [:index]
+  get '/paraphrase', to: "texts#rewrite_text"
+  get '/show-texts', to: "texts#show_texts"
+  get 'delete-texts', to: "texts#delete_texts"
 end
